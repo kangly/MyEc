ca1b0ce8157ae12e22995be470a2bbeaf06ceda7
kangly <371976974@qq.com>
2020-05-04 18:51:32 +0800

    前台首页增加说明

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,7 +7,7 @@ class Index extends Controller
 {
     public function index()
     {
-        $this->assign('content','This is a Index Index，<a href="/admin">访问后台</a>');
+        $this->assign('content','This is a Index Index，<a href="/admin">访问后台</a>，用户名：admin，密码：abcd1234');
 
         return $this->fetch();
     }
